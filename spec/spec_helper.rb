58b2263a9c13fe062ae0edf53e0fd0c556c6ce87
Phil Nash <philnash@gmail.com>
2016-02-13 03:12:53 +0000

    First pass at creating an install command for envyable

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
+require 'fileutils'
 require 'rubygems'
 gem 'minitest'
 require 'minitest/autorun'
 
 require File.join(File.dirname(__FILE__), '..', 'lib', 'envyable')
+
+def destination_root(opts={})
+  dir = File.join(File.dirname(__FILE__), "sandbox")
+  FileUtils.mkdir_p(dir)
+  FileUtils.touch("#{dir}/.gitignore") if opts[:with_gitignore]
+  dir
+end
